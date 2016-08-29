953a4bf9119e5bed53ece8c4bc374a5128cd45ec
Andrei <andreimarin90@gmail.com>
2016-08-29 14:28:10 +0300

    v 1.1.5

--- bbt_framework.php
+++ bbt_framework.php
@@ -3,7 +3,7 @@
  * Plugin Name: BBT Framework
  * Plugin URI: http://bigbangthemes.com/
  * Description: BBTFramework plugin part (registers custom post types, shortcodes and other features of the theme required to be in a plugin)
- * Version: 1.1.4
+ * Version: 1.1.5
  * Author: BigBangThemes
  * Author URI: http://bigbangthemes.com/
  * License: GPL2
