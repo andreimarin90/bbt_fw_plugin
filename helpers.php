c6c25cb0fbd29d90c178fa51c3d4500e3072ab50
Andrei Marin <andreimarin@192-168-0-101.rdsnet.ro>
2016-10-21 12:46:09 +0300

    adding php echoing in toco to plugin

--- helpers.php
+++ helpers.php
@@ -46,3 +46,32 @@ function bbt_category_builder_ids($builder_posts)
 
     return array_unique($builder_categories);
 }
+
+
+/* =====================
+Add php code in Toco custom Code area like this: [bbt_insert_php] bla bla [/bbt_insert_php]
+======================== */
+
+if( ! function_exists('bbt_insert_php_code') )
+{
+
+    function bbt_insert_php_code($content)
+    {
+        $will_bontrager_content = $content;
+        preg_match_all('!\[bbt_insert_php[^\]]*\](.*?)\[/bbt_insert_php[^\]]*\]!is',$will_bontrager_content,$will_bontrager_matches);
+        $will_bontrager_nummatches = count($will_bontrager_matches[0]);
+        for( $will_bontrager_i=0; $will_bontrager_i<$will_bontrager_nummatches; $will_bontrager_i++ )
+        {
+            ob_start();
+            eval($will_bontrager_matches[1][$will_bontrager_i]);
+            $will_bontrager_replacement = ob_get_contents();
+            ob_clean();
+            ob_end_flush();
+            $will_bontrager_content = preg_replace('/'.preg_quote($will_bontrager_matches[0][$will_bontrager_i],'/').'/',$will_bontrager_replacement,$will_bontrager_content,1);
+        }
+        return $will_bontrager_content;
+    } # function will_bontrager_insert_php()
+
+    add_filter( 'the_content', 'bbt_insert_php_code', 9 );
+
+}
